8a7cc0056742afca74209fca1955eb74e379d35c
TheMaverick7 <110233232+theMaverick7@users.noreply.github.com>
2025-03-04 10:58:50 +0000

    fix product model

--- models/product.model.js
+++ models/product.model.js
@@ -1,55 +1,58 @@
-import mongoose from 'mongoose';
-import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+import mongoose from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
 
-const reference = mongoose.Schema.Types.ObjectId;
+const reference = mongoose.Schema.Types.ObjectId
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
-    },
-    quality: {
-        type: reference,
-        ref: 'Quality',
-        required: true
-    },
-    category: {
-        type: reference,
-        ref: 'Category',
-        required: true
-    },
-    price: {
-        type: reference,
-        ref: "Price"
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     inStock: {
-        type: Boolean,
-        required: true
+      type: Boolean,
+      required: true,
     },
     pictures: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     videos: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
+    },
+    price: {
+      type: number,
+      required: true,
+    },
+    quality: {
+      type: String,
+      enum: ["premium", "standard", "economy"],
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: ["fruits", "vegetables", "dairy", "legumes", "meat"],
+      required: true,
     },
     createdBy: {
-        type: reference,
-        ref: 'Farm'
+      type: reference,
+      ref: "Farm",
     },
     reviews: [
-        {
-            type: reference,
-            ref: 'Review'
-        }
-    ]
-}, { timestamps: true });
+      {
+        type: reference,
+        ref: "Review",
+      },
+    ],
+  },
+  { timestamps: true }
+)
 
-mongoose.plugin(mongooseAggregatePaginate);
+mongoose.plugin(mongooseAggregatePaginate)
 
-export const Product = mongoose.model('Product', productSchema);
+export const Product = mongoose.model("Product", productSchema)
--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,101 +1,91 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { apiError } from "../utils/apiError.js";
-import { apiResponse } from "../utils/apiResponse.js";
-import { Product } from "../../models/product.model.js";
-import { Quality } from "../../models/quality.model.js";
-import { Category } from "../../models/category.model.js";
-import { Price } from "../../models/price.model.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import { upload } from "../middlewares/multer.middleware.js";
-import { Farm } from "../../models/farm.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { apiError } from "../utils/apiError.js"
+import { apiResponse } from "../utils/apiResponse.js"
+import { Product } from "../../models/product.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { upload } from "../middlewares/multer.middleware.js"
+import { Farm } from "../../models/farm.model.js"
 
-const createProduct = asyncHandler(async(req, res) => {
-    try {
-    
-    const {
-        name,
-        quality,
-        category,
-        price,
-        description,
-        inStock
-    } = req.body;
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    const { name, quality, category, price, description, inStock } = req.body
 
-    const { id } = req.params;
+    const { id } = req.params
 
-    const fieldEmpty = [name, quality, category, price, description, inStock]
-    .some((elem) => elem.trim() === '');
+    const fieldEmpty = [
+      name,
+      quality,
+      category,
+      price,
+      description,
+      inStock,
+    ].some((elem) => elem.trim() === "")
 
-    if(fieldEmpty) throw new apiError('all fields required');
+    if (fieldEmpty) throw new apiError("all fields required")
 
-    const existedProd = await Product.findOne({name});
+    const existedProd = await Product.findOne({ name })
 
-    if(existedProd) throw new apiError('Product already exist');
+    if (existedProd) throw new apiError("Product already exist")
 
-    let localPicturesPath, localVideosPath;
+    let localPicturesPath, localVideosPath
 
-    if(req.files && Array.isArray(req.files.pictures) && req.files.pictures.length > 0)
-    localPicturesPath = req.files.pictures[0].path;
+    if (
+      req.files &&
+      Array.isArray(req.files.pictures) &&
+      req.files.pictures.length > 0
+    )
+      localPicturesPath = req.files.pictures[0].path
 
-    if(req.files && Array.isArray(req.files.videos) && req.files.videos.length > 0)
-    localVideosPath = req.files.videos[0].path;
+    if (
+      req.files &&
+      Array.isArray(req.files.videos) &&
+      req.files.videos.length > 0
+    )
+      localVideosPath = req.files.videos[0].path
 
-    if(!localPicturesPath) throw new apiError(400,'No local pictures path');
-    if(!localVideosPath) throw new apiError(400, 'No local videos path');
+    if (!localPicturesPath) throw new apiError(400, "No local pictures path")
+    if (!localVideosPath) throw new apiError(400, "No local videos path")
 
-    const thePrice = await Price.findById(createdPrice._id);
-    const theQuality = await Quality.findById(createdQuality._id);
-    const theCategory = await Category.findById(createdCategory._id);
-    const theFarm = await Farm.findById(id);
+    // const thePrice = await Price.findById(createdPrice._id);
+    // const theQuality = await Quality.findById(createdQuality._id);
+    // const theCategory = await Category.findById(createdCategory._id);
+    const theFarm = await Farm.findById(id)
 
-    if(!thePrice || !theQuality || !theCategory || !theFarm) throw new apiError(500);
+    if (!theFarm) throw new apiError(500, "Farm not found")
 
-    let picturesUpload = await uploadOnCloudinary(localPicturesPath);
-    let videosUpload = await uploadOnCloudinary(localVideosPath);
+    // if (!thePrice || !theQuality || !theCategory || !theFarm)
+    //   throw new apiError(500)
 
-    if(!picturesUpload) throw new apiError('pictures upload failed');
-    if(!videosUpload) throw new apiError('videos upload failed');
+    let picturesUpload = await uploadOnCloudinary(localPicturesPath)
+    let videosUpload = await uploadOnCloudinary(localVideosPath)
+
+    if (!picturesUpload) throw new apiError("pictures upload failed")
+    if (!videosUpload) throw new apiError("videos upload failed")
 
     const createProd = await Product.create({
-        name,
-        description,
-        inStock,
-        pictures: picturesUpload.url || '',
-        videos: videosUpload.url || '',
-        createdBy: theFarm._id
-    });
+      name,
+      description,
+      inStock,
+      price,
+      quality,
+      category,
+      pictures: picturesUpload.url || "",
+      videos: videosUpload.url || "",
+      createdBy: theFarm._id,
+    })
 
-    const thisProduct = await Product.findById(createProd._id);
+    const thisProduct = await Product.findById(createProd._id)
 
-    if(!thisProduct) throw new apiError(500, 'Error creating product');
+    if (!thisProduct) throw new apiError(500, "Error creating product")
 
-    const createdPrice = await Price.create({
-        value: price,
-        product: thisProduct._id
-    });
+    console.log("Product Created Successfully", thisProduct)
 
-    const createdQuality = await Quality.create({
-        name: quality.toLowerCase(),
-        product: thisProduct._id
-    });
-
-    const createdCategory = await Category.create({
-        name: category,
-        product: thisProduct._id
-    });
-
-    if(!createdPrice || !createdQuality || !createdCategory)
-    throw new apiError(500, 'Product creation failed');
-
-    console.log('Product Created Successfully', thisProduct);
-
-    return res.status(200).json(
-        new apiResponse(200, thisProduct, 'Product created successfully')
-    );
-
-    } catch (error) {
-        console.log(error)
-    }
+    return res
+      .status(200)
+      .json(new apiResponse(200, thisProduct, "Product created successfully"))
+  } catch (error) {
+    console.log(error)
+  }
 })
 
-export {createProduct};
+export { createProduct }
